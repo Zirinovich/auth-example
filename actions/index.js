231fc6c3de862b6613b1b2d3534a0f88ca0b80bc
Zirinovich <zhorzhmail@gmail.com>
2017-02-01 01:46:49 +0300

    Пример auth
    не работат в таком виде, устарел
    нужен только как пример кода

---
+++ actions/index.js
@@ -0,0 +1,8 @@
+import {loginUser, LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE} from 'loginActions';
+import {logoutUser, LOGOUT_REQUEST, LOGOUT_SUCCESS, LOGOUT_FAILURE} from 'logoutActions';
+import {fetchQuote, fetchSecretQuote, QUOTE_REQUEST, QUOTE_SUCCESS, QUOTE_FAILURE} from 'apiActions';
+
+export {loginUser, logoutUser, fetchQuote, fetchSecretQuote}
+export {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE}
+export {LOGOUT_REQUEST, LOGOUT_SUCCESS, LOGOUT_FAILURE}
+export {QUOTE_REQUEST, QUOTE_SUCCESS, QUOTE_FAILURE}
